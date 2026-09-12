07157b797ed6ddd75b8522af237d40ebf6737e44
frRitamDas <ritamdas20012@gmail.com>
2026-09-12 19:27:01 +0530

    Remove addon and plugin settings from Pixbar

--- composeApp/src/commonMain/kotlin/com/nuvio/app/features/settings/ContentDiscoverySettingsPage.kt
+++ composeApp/src/commonMain/kotlin/com/nuvio/app/features/settings/ContentDiscoverySettingsPage.kt
@@ -1,15 +1,6 @@
 package com.nuvio.app.features.settings
 
 import androidx.compose.foundation.lazy.LazyListScope
-import com.nuvio.app.core.build.AppFeaturePolicy
-import nuvio.composeapp.generated.resources.Res
-import nuvio.composeapp.generated.resources.compose_settings_page_addons
-import nuvio.composeapp.generated.resources.compose_settings_page_plugins
-import nuvio.composeapp.generated.resources.settings_content_discovery_addons_description
-import nuvio.composeapp.generated.resources.settings_content_discovery_addons_description_appstore
-import nuvio.composeapp.generated.resources.settings_content_discovery_plugins_description
-import nuvio.composeapp.generated.resources.settings_content_discovery_section_sources
-import org.jetbrains.compose.resources.stringResource
 
 internal fun LazyListScope.contentDiscoveryContent(
     isTablet: Boolean,
@@ -17,33 +8,6 @@ internal fun LazyListScope.contentDiscoveryContent(
     onAddonsClick: () -> Unit,
     onPluginsClick: () -> Unit,
 ) {
-    item {
-        SettingsSection(
-            title = stringResource(Res.string.settings_content_discovery_section_sources),
-            isTablet = isTablet,
-        ) {
-            SettingsGroup(isTablet = isTablet) {
-                SettingsNavigationRow(
-                    title = stringResource(Res.string.compose_settings_page_addons),
-                    description = stringResource(
-                        if (AppFeaturePolicy.personalMediaAddonCopyEnabled) {
-                            Res.string.settings_content_discovery_addons_description_appstore
-                        } else {
-                            Res.string.settings_content_discovery_addons_description
-                        },
-                    ),
-                    isTablet = isTablet,
-                    onClick = onAddonsClick,
-                )
-                if (showPluginsEntry) {
-                    SettingsNavigationRow(
-                        title = stringResource(Res.string.compose_settings_page_plugins),
-                        description = stringResource(Res.string.settings_content_discovery_plugins_description),
-                        isTablet = isTablet,
-                        onClick = onPluginsClick,
-                    )
-                }
-            }
-        }
-    }
+    // Pixbar uses its built-in TMDB metadata/backend pipeline. Addon and plugin
+    // installation is intentionally no longer exposed in the settings UI.
 }
